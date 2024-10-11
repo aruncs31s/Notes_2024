85e95803cc53db4e776f9537b2a263c0727c2e58
Arun CS <aruncs31ss@gmail.com>
2024-10-12 02:23:41 +0530

    Backup

--- Ethical_Hacking/Scripts/git-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git pull
-git add -A
-
-git commit -m "update README.md"
-
-git push origin main
--- Ethical_Hacking/Scripts/sumof2numbers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Enter the first Number"
-read number1
-echo "Enter the second Number"
-read number2
-echo "sum = " $((number1 + number2))
